b79cffb0af3419c021ff43de074cdd1697f6b20b
qianfeng <qianfeng@SG.local>
2015-12-30 14:53:05 +0800

    Dog.h&&Dog.m

---
+++ 自定义JSon字符串/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  JSon解析
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 qianfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ 自定义JSon字符串/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  JSon解析
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright © 2015年 qianfeng. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
